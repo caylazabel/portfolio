852a2a3b15b0c856ec9981d0cb7416979defc3ed
caylazabel <caylazabel@gmail.com>
2017-01-05 10:25:00 -0800

    added html and functionality for links

--- Scripts/arrayArticles.js
+++ Scripts/arrayArticles.js
@@ -7,7 +7,7 @@ var projectsArray = [
     author: 'Cayla Zabel',
     projectLink:'https://caylazabel.github.io/salmon_cookie_project/',
     publishedOn: '2016-12-12',
-    body: '<p>Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.</p>'
+    body: '<p>Gives both the client a front and back end look at their business of Salmon Cookie Sales. The front end is customer friendly which includes history of the business, locations, and merchandise to be purchased. The back end is for the client to see which allows them to see the total daily sales for the day, which hours sell how many cookies, and how many cookies each location sells. It also allows the client to use a form to put in prosepctive locations and see how sales might be.</p>'
   },
   {
     title: 'Bus Mall',
@@ -15,7 +15,7 @@ var projectsArray = [
     author: 'Cayla Zabel',
     projectLink: 'https://caylazabel.github.io/bus_mall/',
     publishedOn: '2016-12-19',
-    body: '<p>Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.</p>'
+    body: '<p>Bus mall is used as a servey for the general public who might use public transportation. It goes through and bank of images which are shown to the user and they are to choose which product they are most likely to buy.</p>'
   },
   {
     title: 'Find Sam',
@@ -23,6 +23,6 @@ var projectsArray = [
     author: 'Cayla Zabel, Remil Marzan, Khalid Mohamud, Adrian Valmont',
     projectLink: 'https://remilonwheels.github.io/find-sam/',
     publishedOn: '2016-12-23',
-    body: '<p>Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.</p>'
+    body: '<p>A memory game built for a 201 final project at Code Fellows. The game allows the user to enter a username, play the game and then see a leaderboard which displays high scores. This game also included an about us for the deveopers of the game.</p>'
   },
 ];
