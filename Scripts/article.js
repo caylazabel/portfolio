c88b92c53ba0c78bc28f31a59cc91385df8b8f57
caylazabel <caylazabel@gmail.com>
2017-01-05 22:12:02 -0800

    added a bunch of things

--- Scripts/article.js
+++ Scripts/article.js
@@ -3,40 +3,41 @@
 var articles = [];
 
 function Article (works) {
-  this.author = works.author;
-  this.category = works.category;
-  // this.imageUrl = works.imageUrl;
-  this.projectLink = works.projectLink;
-  this.publishedOn = works.publishedOn;
-  this.body = works.body;
-  this.title = works.title;
+  for(var key in works){
+    this[key] = works[key];
+  }
 }
 
-Article.prototype.toHtml = function () {
-  var $newArticle = $('article.template').clone();
-  $newArticle.attr('data-category', this.category);
-  $newArticle.find('a').html(this.author);
-  $newArticle.find('a')
-  $newArticle.find('h1 a')
-              .html(this.title)
-              .attr('href', this.projectLink);
-  $newArticle.find('.article-body').html(this.body);
-  $newArticle.find('time[pubdate]').attr('title',this.publishedOn);
-  $newArticle.find('time').text('about ' + parseInt((new Date() - new Date(this.publishedOn))/60/60/24/1000) + ' days ago');
+Article.prototype.toHtml = function(){
+var source = $('#article-template').html();
+var templateRender = Handlebars.compile(source);
+var content = articles;
+return templateRender(this);
+}
 
-  $newArticle.removeAttr('class');
+// this.daysAgo = parseInt((new Date() - new Data(this.publishedOn))/60/60/24/1000);
+// this.publishStatus = this.publishedOn ? `published ${this.daysAgo} days ago` : '(draft)';
 
-  return $newArticle;
-};
 
-projectsArray.sort(function(currentObject, nextObject){
-  return(new Date(nextObject.publishedOn)) -(new Date(currentObject.publishedOn));
+projectsArray.sort(function(nextObject,currentObject) {
+  return(new Date(nextObject.publishedOn)) - (new Date(currentObject.publishedOn));
 });
 
 projectsArray.forEach(function(ele) {
   articles.push(new Article(ele));
 });
 
-articles.forEach(function(article) {
-  $('#articles').append(article.toHtml());
-});
+articles.forEach(function(a) {
+  $('#articles').append(a.toHtml());
+})
+
+worksViewed.handleMainNav = function(){
+  $('.aboutme')
+  $('.nav-bar').on('click', '.nav-list', function(e){
+    e.preventDefault();
+    $('.tab-content').hide();
+    $('#' + $(this).attr('data-content')).fadeIn();
+  })
+}
+
+worksViewed.handleMainNav();
