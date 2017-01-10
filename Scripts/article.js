4970f1a8f26a016d4b3f11ddc0c60623457763d8
caylazabel <caylazabel@gmail.com>
2017-01-09 17:40:25 -0800

    FINALLLLLY WORKING w local storage

--- Scripts/article.js
+++ Scripts/article.js
@@ -1,6 +1,6 @@
 'use strict';
 
-var articles = [];
+// var articles = [];
 
 function Article (works) {
   for (var key in works){
@@ -8,26 +8,57 @@ function Article (works) {
   }
 }
 
-Article.prototype.toHtml = function(){
-var source = $('#article-template').html();
-var templateRender = Handlebars.compile(source);
+Article.all = [];
 
+Article.prototype.toHtml = function(){
+  let template =
+Handlebars.compile($('#article-template').text());
 
 
 this.daysAgo = parseInt((new Date() - new Date(this.publishedOn))/60/60/24/1000);
 this.publishStatus = this.publishedOn ? `published ${this.daysAgo} days ago` : '(draft)';
+// this.body = marked(this.body);
 
-return templateRender(this);
+return template(this);
 };
 
-projectsArray.sort(function(a,b) {
-  return(new Date(b.publishedOn)) - (new Date(a.publishedOn));
-});
+Article.loadAll = function(rawData) {
+  rawData.sort(function(a,b) {
+    return (new Date(b.publishedOn)) - (new Date(a.publishedOn));
+  })
 
-projectsArray.forEach(function(ele) {
-  articles.push(new Article(ele));
-});
+rawData.forEach(function(ele) {
+  Article.all.push(new Article(ele));
+})
+}
 
-articles.forEach(function(a) {
-  $('#projects').append(a.toHtml());
+
+ Article.fetchAll = function() {
+   if (localStorage.rawData) {
+     var parsedData = JSON.parse(localStorage.rawData);
+     Article.loadAll(parsedData);
+     worksViewed.initIndexPage();
+
+   } else {
+
+$.ajax({
+  type: 'GET',
+  url: '/data/works.json'
+}).done(function(data) {
+  localStorage.works = JSON.stringify(data);
+  Article.loadAll(data);
+  worksViewed.initIndexPage();
 });
+   }
+ };
+// projectsArray.sort(function(a,b) {
+//   return(new Date(b.publishedOn)) - (new Date(a.publishedOn));
+// });
+//
+// projectsArray.forEach(function(ele) {
+//   articles.push(new Article(ele));
+// });
+//
+// articles.forEach(function(a) {
+//   $('#projects').append(a.toHtml());
+// });
